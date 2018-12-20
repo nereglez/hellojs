2288b6109239876bf51684c62478763d47e4c240
Nerea González López <ngonzalez@red.cor>
2018-12-20 18:26:45 +0100

    generators

--- es6.js
+++ es6.js
@@ -1,7 +1,27 @@
 // Arrow functions
+// scope this
 
 const myfunction = () => {
+  let _this = this;
+  [1, 2].forEach(element => {
+    console.log(this)
+    console.log(_this)
+    console.log(element)
+  })
   console.log('First function in ES6')
 }
 
+const mySimpleFunction = () => console.log('Second function in ES6')
+
+// Generators
+function * calculator (input) {
+  var doubleThat = 2 * (yield (input / 2))
+  var another = yield (doubleThat)
+  return (input * doubleThat * another)
+}
+
 myfunction()
+mySimpleFunction()
+
+const calc = calculator(10)
+calc.next()
